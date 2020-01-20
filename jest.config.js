b794ec185046c9fab5873d4f4041a5d1c293d9f2
D0C702WH0 <jemyplu@gmail.com>
2020-01-20 15:36:13 +0100

    jest tests ok react-toastify css

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,8 @@ module.exports = {
   transform: {
     '^.+\\.js$': 'babel-jest',
     '.+\\.(css|styl|less|sass|scss)$': 'jest-transform-css'
-  }
+  },
+  transformIgnorePatterns: [
+    '<rootDir>/(node_modules)/?!(react-toastify/dist/ReactToastify)+(css)$'
+  ]
 }
