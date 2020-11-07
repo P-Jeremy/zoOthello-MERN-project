6c47c3c2ca68a34bba66d4ce48f731c7784f8335
P-Jeremy <jemyplu@gmail.com>
2020-11-07 22:28:12 +0100

    [REACT] update game on move refacto

--- react/src/components/Game/Game.js
+++ react/src/components/Game/Game.js
@@ -4,8 +4,6 @@ import { Button } from 'react-bootstrap'
 import { ToastContainer, toast } from 'react-toastify'
 import PropTypes from 'prop-types'
 import Board from '../Board/Board'
-import merge from 'lodash/merge'
-import reversi from 'reversi'
 import axios from 'axios'
 import openSocket from 'socket.io-client'
 import Pawn from '../Pawn/Pawn'
@@ -15,8 +13,6 @@ import 'react-toastify/dist/ReactToastify.css'
 const uri = 'http://localhost:3000'
 
 const socket = openSocket(uri)
-
-const Reversi = reversi.Game
 
 const url = `${uri}/api/game`
 
@@ -33,51 +29,26 @@ export default class Game extends Component {
     currentPlayer: ''
   }
 
-  /** MERGE THE RESULT FROM DB INTO A NEW GAME INSTANCE
-   * In order to get all Game class methods
-   * @param res data from the mongo db
-   * @returns game instance of data in state
-   */
-  turnDataInGameInstance = async (res) => {
-    // if (!res.data.length) return
-    const newGame = new Reversi()
-    const dbGame = res.data.gameData.game
-    merge(newGame, dbGame)
-    const gameId = res.data.gameData._id
-
-    await this.setState({
-      gameId,
-      nextPlayer: newGame._nextPieceType,
-      game: newGame,
-      blackPassCount: res.data.gameData.blackPassCount,
-      whitePassCount: res.data.gameData.whitePassCount,
-      blackPlayer: res.data.blackPlayerData,
-      whitePlayer: res.data.whitePlayerData,
-      currentPlayer: res.data.currentPlayer
-    })
-
-    return this.countPoints()
-  }
-
-  /**
-   * FETCH GAME DATA FROM DB
-   * And turn it into Game instance
-   */
   getGameData = (gameId) => {
     axios.get(`${url}/one/${gameId}`).then(res => {
       if (res) {
-        this.turnDataInGameInstance(res)
+        this.setState({
+          gameId: res.data.gameData._id,
+          nextPlayer: res.data.gameData.game._nextPieceType,
+          game: res.data.gameData.game,
+          blackPassCount: res.data.gameData.blackPassCount,
+          whitePassCount: res.data.gameData.whitePassCount,
+          blackPlayer: res.data.blackPlayerData,
+          whitePlayer: res.data.whitePlayerData,
+          currentPlayer: res.data.currentPlayer
+        })
+        return this.countPoints()
       }
     })
       .catch(err => err)
   }
 
-  /**
-   * Allows to create a toaster message depending on the provided payload and
-   * @param {*} payload payload provided by the socket
-   * @returns a toast() implementation with corresponding message
-   */
-  toaster (payload) {
+  handleToaster (payload) {
     if (payload.id !== this.state.gameId) return false
     const hour = new Date().getHours()
     const minute = new Date().getMinutes()
@@ -102,26 +73,22 @@ export default class Game extends Component {
     }
   }
 
-  /** Check if there is a userId in localstorage
-   * @returns the user ID or false
-   */
   isUserAuth () {
     const userId = localStorage.getItem('userId')
     return userId || false
   }
 
-  /** Redirect to home */
-  goHome () {
+  redirectToHome () {
     window.location.href = '/'
   }
 
   componentDidMount () {
     document.title = 'Game'
     const gameId = this.props.match.params.id
-    if (!this.isUserAuth()) this.goHome()
+    if (!this.isUserAuth()) this.redirectToHome()
     this.getGameData(gameId)
     socket.on('gameUpdated', (payload) => {
-      this.toaster(payload)
+      this.handleToaster(payload)
       this.getGameData(gameId)
     })
 
@@ -133,10 +100,6 @@ export default class Game extends Component {
     })
   }
 
-  /**
- * Allows to get the game score
- * set the score state according to countByPieceType() method results
- */
   countPoints = () => {
     const { game } = this.state
     if (game !== null) {
@@ -145,17 +108,12 @@ export default class Game extends Component {
     }
   }
 
-  /**
-   * Update the game in DB
-   */
-  updateGame = ({ origin, player }) => {
+  updateGameOnMove = (coordinates) => {
     const { gameId, game, blackPassCount, whitePassCount } = this.state
+    const userId = localStorage.getItem('userId')
     axios.put(`${url}/update/move/${gameId}`, {
-      whitePassCount,
-      blackPassCount,
-      game,
-      origin,
-      player
+      userId,
+      coordinates
     })
     return this.setState({ nextPlayer: game._nextPieceType, blackPassCount, whitePassCount })
   }
@@ -170,54 +128,13 @@ export default class Game extends Component {
     return this.setState({ nextPlayer: game._nextPieceType, blackPassCount, whitePassCount })
   }
 
-  /**
-   * Check if the actual user is allowed to play
-   * @returns true or false
-   */
-  isUserAllowedToPlay = () => {
-    const userId = localStorage.getItem('userId')
-    const wrongUser = () => {
-      toast.error('Attends ton tour...coin-coin')
-      return false
-    }
-    const { blackPlayer, game, whitePlayer } = this.state
-    switch (game._nextPieceType) {
-      case 'BLACK':
-        if (userId === blackPlayer._id) {
-          return true
-        } else {
-          wrongUser()
-        }
-        break
-      case 'WHITE':
-        if (userId === whitePlayer._id) {
-          return true
-        } else {
-          wrongUser()
-        }
-        break
-      default:
-        break
-    }
-  }
-
-  /**
- * Allows to change the pawn status if game move is legit
- * @param {*} x pawn X coordinate
- * @param {*} y pawn Y coordinate
- */
   handleClick = (x, y) => {
-    const { game, blackPlayer, whitePlayer } = this.state
     toast.dismiss()
-    /* CHECK IF THE USER IS ALLOWED TO PLAY */
-    if (!this.isUserAllowedToPlay()) return
-    /* CHECK IF THE MOVE IS LEGAL */
-    const report = game.proceed(x, y)
-    /* RETURNS IF ILLEGAL MOVE */
-    if (!report.isSuccess) {
-      return
+    const coordinates = {
+      x,
+      y
     }
-    this.updateGame({ origin: 'move', player: game._nextPieceType === 'WHITE' ? blackPlayer.pseudo : whitePlayer.pseudo })
+    this.updateGameOnMove(coordinates)
     return this.countPoints()
   }
 
@@ -225,9 +142,6 @@ export default class Game extends Component {
     return this.updateGameOnPass()
   }
 
-/**
- * Allows to know if "YOU" or the opponent pseudo should be displayed
- */
 getTurnUserName = () => {
   const userId = localStorage.getItem('userId')
   const { whitePlayer, blackPlayer, nextPlayer } = this.state
